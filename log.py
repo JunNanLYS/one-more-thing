5e03bd5c7ad8393729ba64f00b1641aeb4c0d04f
NanJunLYS <18906571516@163.com>
2024-03-08 14:00:24 +0800

    Create log.py

---
+++ log.py
@@ -0,0 +1,29 @@
+import os
+import logging
+from datetime import datetime
+
+import config as cfg
+
+time_ = datetime.now().strftime("%Y-%m-%d")
+prefix = os.path.join(cfg.__abspath__, "logs")
+filename = os.path.join(prefix, f"{time_}.log")
+
+# logging config
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] | %(name)s | %(asctime)s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler(filename),
+        logging.StreamHandler()  # 添加一个StreamHandler来输出到终端
+    ]
+)
+
+logger = logging.getLogger("one-more-thing")
+
+if __name__ == "__main__":
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
