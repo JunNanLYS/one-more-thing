478a9298e5ad5920dd2add43da3addeb18b6dc05
NanJunLYS <18906571516@163.com>
2024-03-19 14:04:51 +0800

    修复单词拼错

--- src/source_data.py
+++ src/source_data.py
@@ -4,24 +4,24 @@ from PyQt6.QtCore import pyqtSignal, QObject
 from PyQt6.QtWidgets import QApplication
 
 from log import logger
-from src.utils.file import JsonDataStorge
+from src.utils.file import JsonDataStorage
 
 
 class SourceData(QObject):
     def __init__(self, path: str, parent=None):
         super().__init__(parent)
         self.path = path
-        self._storge = JsonDataStorge(path)
-        self._storge.dumped.connect(self.dumped)
-        self._storge.loaded.connect(self.loaded)
-        self._storge.load()
+        self._storage = JsonDataStorage(path)
+        self._storage.dumped.connect(self.dumped)
+        self._storage.loaded.connect(self.loaded)
+        self._storage.load()
 
     def isLoaded(self) -> bool:
-        return self._storge.isLoaded()
+        return self._storage.isLoaded()
 
     @property
-    def storge(self) -> JsonDataStorge:
-        return self._storge
+    def storage(self) -> JsonDataStorage:
+        return self._storage
 
     dumped = pyqtSignal()
     loaded = pyqtSignal()
@@ -33,5 +33,5 @@ if __name__ == '__main__':
     d = SourceData(_path)
     d.loaded.connect(lambda: print("loaded"))
     logger.debug("我是主线程")
-    d.storge.dict["name"] = "zhang san"
+    d.storage.dict["name"] = "zhang san"
     app.exec()
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .icon import OMThingIcon
 from .type_cast import isFluentIconStr, strToFluentIcon, fluentIconToStr
 from .screen import getRealScreenSize, getScreenScale, getScreenSize
-from .file import JsonDataStorge
+from .file import JsonDataStorage
