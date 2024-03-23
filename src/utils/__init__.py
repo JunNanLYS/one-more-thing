4701fce9f1c1a44e1d3dab3b18c091eee10d6c89
NanJunLYS <18906571516@163.com>
2024-03-23 16:26:03 +0800

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
-from .icon import OMThingIcon
 from .type_cast import isFluentIconStr, strToFluentIcon, fluentIconToStr
 from .screen import getRealScreenSize, getScreenScale, getScreenSize
 from .file import JsonDataStorage
+from .utils import getLabelBoundingRect, addSubItem, removeSubItem
