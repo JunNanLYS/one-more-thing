2a11f04f6cdfd51f5a0f52d5e835cbbf3aa51620
NanJunLYS <18906571516@163.com>
2024-03-23 16:25:40 +0800

    新增一些工具方法

---
+++ src/utils/utils.py
@@ -0,0 +1,31 @@
+from PyQt6.QtCore import QRect, Qt
+from PyQt6.QtWidgets import QLabel
+
+from log import logger
+from src.py_qobject import PyQDict, PyQList
+
+
+def getLabelBoundingRect(label: QLabel) -> QRect:
+    return label.fontMetrics().boundingRect(label.text())
+
+
+def addSubItem(_dict: PyQDict, item: PyQDict) -> bool:
+    try:
+        _list: PyQList = _dict["subItems"]
+        _list.append(item)
+        item.valueChanged.connect(_list.valueChanged.emit, Qt.ConnectionType.UniqueConnection)
+        return True
+    except Exception as e:
+        logger.error(f"{e}")
+        return False
+
+
+def removeSubItem(_dict: PyQDict, item: PyQDict) -> bool:
+    try:
+        _list = _dict["subItems"]
+        _list.remove(item)
+        item.deleteLater()
+        return True
+    except Exception as e:
+        logger.error(f"{e}")
+        return False
