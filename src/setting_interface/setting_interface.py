606d1488ed922ed3195cffd56020063cd0aa14fe
NanJunLYS <18906571516@163.com>
2024-03-23 16:26:24 +0800

    新增选项卡

--- src/setting_interface/setting_interface.py
+++ src/setting_interface/setting_interface.py
@@ -1,10 +1,11 @@
 import os.path
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QEasingCurve
 from PyQt6.QtWidgets import QWidget, QFileDialog
 from qfluentwidgets import (SettingCardGroup, SmoothScrollArea, ExpandLayout,
                             PushSettingCard, FluentIcon, TitleLabel,
-                            SwitchSettingCard, ComboBoxSettingCard)
+                            SwitchSettingCard, ComboBoxSettingCard,
+                            OptionsSettingCard, setTheme)
 
 import config as cfg
 from src.utils.file import getFilename
@@ -15,10 +16,11 @@ ds = cfg.cfgDS
 class SettingInterface(SmoothScrollArea):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.titleLb = TitleLabel("Setting", self)
+        self.titleLb = TitleLabel("Settings", self)
         self.scrollWidget = QWidget(self)
         self.expandLayout = ExpandLayout(self.scrollWidget)
 
+        # do thing
         self.doThingGroup = SettingCardGroup("Do Thing", self.scrollWidget)
         self.clockBackgroundImage = PushSettingCard(
             title="Clock background image",
@@ -27,7 +29,39 @@ class SettingInterface(SmoothScrollArea):
             text="Set",
             parent=self.doThingGroup
         )
+        self.usePomodoroTime = SwitchSettingCard(
+            title="Enable Pomodoro time",
+            icon=FluentIcon.STOP_WATCH,
+            configItem=ds.usePomodoroTime,
+            parent=self.doThingGroup
+        )
+        self.onePomodoroTime = OptionsSettingCard(
+            title="One Pomodoro time",
+            icon=FluentIcon.STOP_WATCH,
+            content="Set your one Pomodoro time",
+            configItem=ds.onePomodoroTime,
+            texts=["30", "25", "20", "15"],
+            parent=self.doThingGroup
+        )
+        self.pomodoroBreak = OptionsSettingCard(
+            title="Pomodoro break",
+            icon=FluentIcon.STOP_WATCH,
+            content="Set your break time",
+            configItem=ds.pomodoroBreak,
+            texts=["5", "10"],
+            parent=self.doThingGroup
+        )
+        self.afterFourPomodoro = OptionsSettingCard(
+            title="After four Pomodoro time",
+            icon=FluentIcon.STOP_WATCH,
+            content="Set your break time",
+            configItem=ds.afterFourPomodoro,
+            texts=["10", "15", "20", "25", "30"],
+            parent=self.doThingGroup
+        )
+        # do thing end
 
+        # log
         self.logGroup = SettingCardGroup("Log", self.scrollWidget)
         self.outputLog = SwitchSettingCard(
             title="Output log",
@@ -51,6 +85,34 @@ class SettingInterface(SmoothScrollArea):
             texts=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
             parent=self.logGroup
         )
+        # log end
+
+        # personalization
+        self.personalizationGroup = SettingCardGroup("Personalization", self.scrollWidget)
+        self.applicationTheme = OptionsSettingCard(
+            title="Application theme",
+            icon=FluentIcon.BRUSH,
+            content="Set your application theme",
+            configItem=ds.themeMode,
+            texts=["Light", "Dark", "Auto"],
+            parent=self.personalizationGroup
+        )
+        self.language = ComboBoxSettingCard(
+            title="Language",
+            icon=FluentIcon.LANGUAGE,
+            content="Set you language for UI",
+            configItem=ds.language,
+            texts=["English", "简体中文"],
+            parent=self.personalizationGroup
+        )
+        self.useOpenGL = SwitchSettingCard(
+            title="Hardware acceleration",
+            icon=FluentIcon.VIEW,
+            content="Enable hardware acceleration",
+            configItem=ds.useOpenGL,
+            parent=self.personalizationGroup
+        )
+        # personalization end
 
         self._setQss()
         self.__initWidget()
@@ -85,6 +147,7 @@ class SettingInterface(SmoothScrollArea):
     def __connectSignal(self):
         self.clockBackgroundImage.clicked.connect(self.__onClockBICardClicked)
         self.logPath.clicked.connect(self.__onLogPathCardClicked)
+        self.applicationTheme.optionChanged.connect(lambda item: setTheme(item.value))
 
     def __initWidget(self):
         self.setObjectName("SettingInterface")
@@ -92,6 +155,8 @@ class SettingInterface(SmoothScrollArea):
         self.setWidget(self.scrollWidget)
         self.setWidgetResizable(True)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setScrollAnimation(Qt.Orientation.Vertical, 400, QEasingCurve.Type.OutQuint)
+        self.setScrollAnimation(Qt.Orientation.Horizontal, 400, QEasingCurve.Type.OutQuint)
         self.setViewportMargins(0, 80, 0, 20)
         self.__initSettingGroup()
         self.__initLayout()
@@ -99,15 +164,24 @@ class SettingInterface(SmoothScrollArea):
 
     def __initSettingGroup(self):
         self.doThingGroup.addSettingCard(self.clockBackgroundImage)
+        self.doThingGroup.addSettingCard(self.usePomodoroTime)
+        self.doThingGroup.addSettingCard(self.onePomodoroTime)
+        self.doThingGroup.addSettingCard(self.pomodoroBreak)
+        self.doThingGroup.addSettingCard(self.afterFourPomodoro)
 
         self.logGroup.addSettingCard(self.outputLog)
         self.logGroup.addSettingCard(self.logPath)
         self.logGroup.addSettingCard(self.logLevel)
 
+        self.personalizationGroup.addSettingCard(self.applicationTheme)
+        self.personalizationGroup.addSettingCard(self.language)
+        self.personalizationGroup.addSettingCard(self.useOpenGL)
+
     def __initLayout(self):
         self.titleLb.move(36, 30)
         self.expandLayout.addWidget(self.doThingGroup)
         self.expandLayout.addWidget(self.logGroup)
+        self.expandLayout.addWidget(self.personalizationGroup)
 
         self.expandLayout.setSpacing(28)
         self.expandLayout.setContentsMargins(36, 10, 36, 0)
