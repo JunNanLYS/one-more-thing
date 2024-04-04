5444a8e96e9fa07d8356fdfc39b04067b218065c
NanJunLYS <18906571516@163.com>
2024-04-04 18:17:03 +0800

    新增图标选择器

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -2,4 +2,5 @@ from .card_layout import CardLayout
 from .time_picker import TimePicker, getNextHour, getNextMinute, getNextSecond
 from .timer_label import TimerLabel
 from .icon import OMThingIcon, ProjectIcon
+from .icon_picker import IconPicker
 from .music_player import Music
